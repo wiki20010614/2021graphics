f1288b71c1e4dc1784a7578b2d48213f335e1825
wiki20010614 <80247382+wiki20010614@users.noreply.github.com>
2021-05-24 01:53:59 +0800

    Add files via upload

---
+++ 210517_gundam_bg_turn.cpp
@@ -0,0 +1,72 @@
+#include <opencv/highgui.h> ///使用 OpenCV 2.1 比較簡單, 只要用 High GUI 即可
+#include <opencv/cv.h>
+#include <GL/glut.h>
+#include "glm.h"   ///使用外掛
+
+GLMmodel*pmodel1=NULL;   ///glm模型指標
+
+void drawmodel(void)    ///函式讀取鋼彈
+{
+    if(!pmodel1)
+    {
+        pmodel1=glmReadOBJ("Gundam.obj");
+        if(!pmodel1)exit(0);
+        glmUnitize(pmodel1);
+        glmFacetNormals(pmodel1);
+        glmVertexNormals(pmodel1, 90.0);
+    }
+    glmDraw(pmodel1,GLM_SMOOTH | GLM_TEXTURE);   ///參數
+}
+
+GLuint id1, id2; ///TODO:增加2個 貼圖ID
+int myTexture(char * filename)
+{
+    IplImage * img = cvLoadImage(filename); ///OpenCV讀圖
+    cvCvtColor(img,img, CV_BGR2RGB); ///OpenCV轉色彩 (需要cv.h)
+    glEnable(GL_TEXTURE_2D); ///1. 開啟貼圖功能
+    GLuint id; ///準備一個 unsigned int 整數, 叫 貼圖ID
+    glGenTextures(1, &id); /// 產生Generate 貼圖ID
+    glBindTexture(GL_TEXTURE_2D, id); ///綁定bind 貼圖ID
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); /// 貼圖參數, 超過包裝的範圖T, 就重覆貼圖
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT); /// 貼圖參數, 超過包裝的範圖S, 就重覆貼圖
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST); /// 貼圖參數, 放大時的內插, 用最近點
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); /// 貼圖參數, 縮小時的內插, 用最近點
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img->width, img->height, 0, GL_RGB, GL_UNSIGNED_BYTE, img->imageData);
+    return id;
+}
+
+float angle=0;  ///設定初始角度為0
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glBindTexture(GL_TEXTURE_2D, id1);
+    glPushMatrix();   ///開始畫鋼彈
+        glRotatef(angle,0,1,0);   ///軸心旋轉
+        drawmodel();
+    glPopMatrix();
+
+    glBindTexture(GL_TEXTURE_2D, id2);
+    glBegin(GL_POLYGON);
+        glTexCoord2f( 0, 0 ); glVertex3f( -1, +1 ,0.8);
+        glTexCoord2f( 0, 1 ); glVertex3f( -1, -1 ,0.8);
+        glTexCoord2f( 1, 1 ); glVertex3f( +1, -1 ,0.8);
+        glTexCoord2f( 1, 0 ); glVertex3f( +1, +1 ,0.8);
+    glEnd();
+    glutSwapBuffers();
+    angle++;   ///角度不斷增加
+}
+int main(int argc, char** argv)
+{
+    glutInit( &argc, argv );
+    glutInitDisplayMode( GLUT_DOUBLE | GLUT_DEPTH );
+    glutCreateWindow("week13 texture");
+    glutDisplayFunc(display);
+    glutIdleFunc(display);   ///有空就畫並++
+
+    id1 = myTexture("Diffuse.jpg");///鋼彈貼圖
+    id2 = myTexture("shin.png");///背景
+    glEnable(GL_DEPTH_TEST);
+    glutMainLoop();
+}
